9d3b16f21a4c1695c3667bda088dbce032b73677
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-26 10:29:06 +0000

    [expo-server] setup body-parser

--- expo-server/src/index.ts
+++ expo-server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import productsRouter from "./routes/products";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send(`Hello World with port from env file!${process.env.PORT}`);
