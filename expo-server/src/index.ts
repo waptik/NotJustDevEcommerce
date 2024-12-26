daa282b336ebd66a97f383f34ac7ccd7a35ba4ba
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-26 22:10:15 +0000

    [expo-server] replace `body-parser` with first-party middleware from express

--- expo-server/src/index.ts
+++ expo-server/src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express, { json, urlencoded } from "express";
 
 import productsRouter from "./routes/products";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
 app.get("/", (req, res) => {
     res.send(`Hello World with port from env file!${process.env.PORT}`);
