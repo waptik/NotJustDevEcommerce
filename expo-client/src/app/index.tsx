6ef24cb862000b292ae33022f6b7736c17737dc3
TheVirginBrokey <1687551+waptik@users.noreply.github.com>
2024-12-28 09:21:27 +0000

    [expo-client] setup expo-router

---
+++ expo-client/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+	return (
+		<View>
+			<Text style={{ fontSize: 30, color: "red" }}>Home Screen</Text>
+		</View>
+	);
+}
